4b8e6672592530eb7f77c6d743c0593bb21b1f63
“Zhengren <2327974j@student.gla.ac.uk>
2019-02-08 16:33:33 +0000

    chapter 7

--- rango/models.py
+++ rango/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.template.defaultfilters import slugify
 
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
+    max_name_length = 128
+    name = models.CharField(max_length=max_name_length, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField()
@@ -18,8 +19,9 @@ class Category(models.Model):
         return self.name
 
 class Page(models.Model):
+    max_title_length = 128
     category = models.ForeignKey(Category)
-    title = models.CharField(max_length=128)
+    title = models.CharField(max_length=max_title_length)
     url = models.URLField()
     views = models.IntegerField(default=0)
     
--- rango/urls.py
+++ rango/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
-from rango import views
 
+@@ -4,5 +4,7 @@
 urlpatterns = [
                url(r'^$', views.index, name='index'),
                url(r'^about/$', views.about, name='about'),
+               url(r'^add_category/$', views.add_category, name='add_category'),
                url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category'),
+               url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
                ]
-
