e27da5519724e29a24391dbb1271ed1bd7859e8e
“Zhengren <2327974j@student.gla.ac.uk>
2019-02-08 12:20:52 +0000

    first commit

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def homepage(requeest):
+    return HttpResponse("Homepage"
+                        "<br>"
+                        "<a href='/rango/'>Index</href>"
+                        "<br>"
+                        "<a href='/rango/about/'>About</href>")
+
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=context_dict)
 
-
+def about(request):
+    context_dict = {'author': "Zhengren Jing (2327974J)"}
+    return render(request, 'rango/about.html', context=context_dict)
